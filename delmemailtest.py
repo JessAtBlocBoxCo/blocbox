a2534a0e6f40c2eb86afa603c6798d1e9ed7214b
jess <jess@blocbox.co>
2014-08-16 14:27:40 -0700

    commiting with newly added files

---
+++ delmemailtest.py
@@ -0,0 +1,5 @@
+from django.core.mail import send_mail
+
+send_mail('Subject here - test test', 'This is a test message from my gmail (first address) to admin@blocbox.co (second).', 
+					'jessica.yeats@gmail.com',
+    ['admin@blocbox.co'], fail_silently=False)
