e02cda5900e486b23a64aa0de4a6ed6a69291918
jess <jess@blocbox.co>
2014-08-17 03:43:00 -0700

    updating email_host_password

--- blocbox/settings.py
+++ blocbox/settings.py
@@ -97,7 +97,7 @@ STATICFILES_DIRS = (
 #Email Settings
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = 'admin@blocbox.co'
-EMAIL_HOST_USER = 'GoodHood123'
+EMAIL_HOST_PASSWORD = 'GoodHood123'
 DEFAULT_FROM_EMAIL = 'admin@blocbox.co'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
