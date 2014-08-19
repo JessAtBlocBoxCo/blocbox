ca7a161e782de78b493296c8561bb39caa5f5ddf
jess <jess@blocbox.co>
2014-08-18 17:02:43 -0700

    updating todo list

--- blocbox_todo.py
+++ blocbox_todo.py
@@ -1,12 +1,17 @@
-
-1. fix the admin site so that you can change user info
+1. EDIT host profile page so it looks different if youre  connnecte d- like {%if registered} 
+1. fix the "duplicate key" when they request to connect to someone connecte dto - have it return an erro
+2. edit "Thank you for registering" page to say "an email has been sent to the host to confirm your request to connect - you will be notified when they accept"
+3. change admin@blocbox.co "from" name when they send the request to connect - make it prettier
+1. add hyperlinke to the email send to the person when request to connect so they ca n accept, or deny
 2. add a forgot password/chnage password
 3. send a thank you/confirmation email to USER when they sign up
-4. register core_userinfo_neighbors with the admin site (if this is possible - not its own model)
-5. add hyperlinke to the email send to the person when request to connect so they ca n accept, or deny
-6. notify the user that their request to connect has been confirmed - through an email
+4. notify the user that their request to connect has been confirmed - through an email
+
 3. ADD FIELD: phone_number
 3. create host table - subset of core_userinfo
 4. create pickup_from and pickup_to, see if we can use time field types
-5. forgot password/ password reset
-6. fix the bug in the admin site
+
+
+#Long Term
+--Fix the admin site/ can be more intuitive/cleaner
+--Particularly need to updat the Connections mode view in admin - want to see the connectiosn from each host
