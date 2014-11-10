ab42a3aed248629950e97f2cb13eb7c5caa21c16
jess <jess@blocbox.co>
2014-11-09 23:55:35 -0800

    testing hpv

--- core/views.py
+++ core/views.py
@@ -44,8 +44,11 @@ def dashboard(request, userinfo_id):
 def hostprofile(request, userinfo_id):
     context = RequestContext(request)
     host = get_object_or_404(UserInfo, pk=userinfo_id)
+    #enduser = get_object_of_404(UserInfo, pk=
+    #connected = Connection.objects.are_neighbors(user1=host, user2=) #true of false, but not sure how to call thee user...
     connections_all = Connection.objects.filter(host_user=host) 
     connections_count = Connection.objects.filter(host_user=host).count() #count them,removing status=0 after host_user=host
+    
     transactions_all = Transaction.objects.filter(payee=host)
     transactions_count = Transaction.objects.filter(payee=host).count() #count all of the transactions
     return render_to_response('blocbox/host-profile.html', {'host':host, 
