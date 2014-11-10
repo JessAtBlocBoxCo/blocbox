117632aa2a6c50198a941181d585a78122651cbc
JB <john@blocbox.co>
2014-11-10 01:31:40 -0500

    todo

--- blocbox_todo.py
+++ blocbox_todo.py
@@ -3,57 +3,45 @@
 
 """WEBSITE WORK: JESS TESTING ANOTHER CHANGE"""
 
-1. JESS HPV 				
+1. JESS HPV
 			-- Add "wghen i'm home" field..... connect to availability
- 		-- to userinfo: whenimhome_days  
+ 		-- to userinfo: whenimhome_days
  										whenimhome_hours
- 			
+
 				make different for connected vs not:
-				
-				2. verified info: if not connected, say "Verified Info" -- 
+
+				2. verified info: if not connected, say "Verified Info" --
 				3. when connected to the user, dont say "sign up to connect" -so edit that languager
-2. JESS HPV - rather tha 'If authenticated' show approx address etc.. say if connected.. remember how to do that				
+2. JESS HPV - rather tha 'If authenticated' show approx address etc.. say if connected.. remember how to do that
 11. JESS: dashboard
 			-- create a field for profilepic links - the NULL/DEFAULT value is images/profilepics/blankprofilepic.png
 --- connect to model/frields: phone_number, neighborhood, st_address1, st_address2, zipcode, city, state
 
 12. JOHN HPV responsibe: whole site needs addressed, address field and name underpicture are not responsive
 13. JOHN waitlist: fix the colors
-15. JOHN dashbaord: add a "start a shipment" action button or link or wahtever to dashboard
+
 15. JOHN waitlist (index.html): fix fucked up color stuff
 16. JESS: beta - toggle button
-16. JOHN: startashipment - 
-		
-		-- select ta host - merge thea ddress into drop-down .. wider button - and get rid of the address and get directions
-			-- have button say "select" and have an option..for john, with address, have an option "select host"
-			-- consider resizing text
-			-- confirm availability - fill out drop down menu (fedex, ups, etc)..
-				--confirem availability - ONE dropdown - e.g., "fedex, 2 day", "USPS priority (2 day", usps 7-10 days.. 
-				-- john add note around aviability: "Here's our avaiability policy: hosts are required to regularly update their availability.  
-																							Hosts are accountable for all packages delivered on days that they commmited to being available.  
-																							If they miss the package, you will not be charged, and the host is responsible for tracking down the missed delivery.
-																							However, if you send a packaget to a host that might arrive on a day when they have stated they are unavailable,
-																							you will be charged even if the host misses the delivery.  You're likely to still get the package, but it may not be available for pickup 
-																							until a later date." 
-																							
+
+
 
 				- add a hyperlink to the payment page when the user "submits"
 19. JOHN: payment pagee
 			-- update payment options in first block
 			-- add link to shipapackage from the payment page
-20. JESS: payment page -- do the django app for payment				
+20. JESS: payment page -- do the django app for payment
 21. JESS startashipment: add a django calendar app
 22. JOHN: you send jess a message saying "package received"
-23. JOHN shippackage: add weights
+
 24. JESS: shippackage: connect the host address fields to data table so populate,
 25. JESS: send a user an email when they ship package
 26. JESS: messagig system figure out how to integratem with email/so messages are sent as emails
-1. JOHN: add his profile picture to HPV.
+
 2. JOHN: fix mobile
 	3. JESS to form and table - add need_meals
 20. JOHN MYBLOCK: code a basic version of this
 12. JESS MYBLOCK: fill this in = sjpw who you are connected to- just do basic list
-3. JOHN search page: 
+3. JOHN search page:
 		-- make text look better for john
 			-"dont see a blocbox host in your hood"? -- MAKE IT LARGER
 """WEBSITE TASKS TO DO AFTER TUMML APP"""
@@ -87,7 +75,7 @@
 """PITCH WORK"""
 #1. make an outline
 #2. make the pitch
-#3. 
+#3.
 
 
 
@@ -105,7 +93,7 @@
 
 #6. Navbar/ drop downs
 
-#7. Review stock questions (on gdrive) to ensure we have answers to all. NOT suggesting we write answers. We’ll review them this weekend.
+#7. Review stock questions (on gdrive) to ensure we have answers to all. NOT suggesting we write answers. Weï¿½ll review them this weekend.
 
 
 
@@ -119,7 +107,7 @@
 
 6. Email Confirmation (not a page)
 
-(user receives confirmation email indicating the host has accepted them. Links in email to “Start a shipment with John”, “John’s Host Profile Page”, Your dashboard”? )
+(user receives confirmation email indicating the host has accepted them. Links in email to ï¿½Start a shipment with Johnï¿½, ï¿½Johnï¿½s Host Profile Pageï¿½, Your dashboardï¿½? )
 
 
 
@@ -200,7 +188,7 @@ Host Address
 		Recent Shipments (tracking number, host, host address, delivery date)
 """
 
-#add a field to the user_info that shows the first host the user connected to - NULl is OK for this b/c sometimes wont have connected to anyone... 
+#add a field to the user_info that shows the first host the user connected to - NULl is OK for this b/c sometimes wont have connected to anyone...
 
 #. finish paypal/payment thing
 https://github.com/spookylukey/django-paypal
@@ -237,12 +225,12 @@ def view_that_asks_for_money(request):
 
 1. create superuser accounts for john, also maybe me so we dont need to log in as root
 2. clean up messaging system - create structure/dashboard/
-3. figur eout django-dash/dashboard app 
+3. figur eout django-dash/dashboard app
 
 
 
 
-1. EDIT host profile page so it looks different if youre  connnecte d- like {%if registered} 
+1. EDIT host profile page so it looks different if youre  connnecte d- like {%if registered}
 
 3. START A SHIPMENT process -if connected - start programming that
 
@@ -256,7 +244,7 @@ def view_that_asks_for_money(request):
 --Particularly need to updat the Connections mode view in admin - want to see the connectiosn from each host
 -- ADD FIELD: phone_number
 -- embed SMS notificaitons
-1. Approximate Address right now we have address in... it should be a APPROXIMATE ADDRESS, : address_approx, neighborhood, city, state 
+1. Approximate Address right now we have address in... it should be a APPROXIMATE ADDRESS, : address_approx, neighborhood, city, state
 					say "cross-strees... connect with john to see address"
 
 
@@ -280,13 +268,28 @@ So when you push to origin, it will push to both repositories
 
 
 #done
-1. transfer the host for blocbox.co to the digitalocean server: 
+1. transfer the host for blocbox.co to the digitalocean server:
 2. add a forgot password/chnage password
 1. JESS: beta.html: fix that shit with the margin padding on right-hand side
 			- add "start a shipment" to navbar
 			- nabbar needs to be drop0down is totally fucked - the list shows up to left
 			HPV-- remove "this is test host_aboutme message" - change the text a bit
-			
+
 2. JESS: base.html -  Navbar  navbar height should be bigger on desktop
 		--JESS HPV - count number of connections... - - MAKE A FUNCTION TO COUNT NEIGHBORS YOU ARE HELPING
-	
+1. JOHN: add his profile picture to HPV.
+23. JOHN shippackage: add weights
+16. JOHN: startashipment -
+
+		-- select ta host - merge thea ddress into drop-down .. wider button - and get rid of the address and get directions
+			-- have button say "select" and have an option..for john, with address, have an option "select host"
+			-- consider resizing text
+			-- confirm availability - fill out drop down menu (fedex, ups, etc)..
+				--confirem availability - ONE dropdown - e.g., "fedex, 2 day", "USPS priority (2 day", usps 7-10 days..
+				-- john add note around aviability: "Here's our avaiability policy: hosts are required to regularly update their availability.
+																							Hosts are accountable for all packages delivered on days that they commmited to being available.
+																							If they miss the package, you will not be charged, and the host is responsible for tracking down the missed delivery.
+																							However, if you send a packaget to a host that might arrive on a day when they have stated they are unavailable,
+																							you will be charged even if the host misses the delivery.  You're likely to still get the package, but it may not be available for pickup
+																							until a later date."
+15. JOHN dashbaord: add a "start a shipment" action button or link or wahtever to dashboard
