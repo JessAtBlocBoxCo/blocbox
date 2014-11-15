4e8c8b1bdd155c9017cbcb6aab754356d26eb388
jess <jess@blocbox.co>
2014-11-15 01:40:45 -0800

    testing app

--- testing/views.py
+++ testing/views.py
@@ -1,3 +1,129 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, render_to_response
+from django.http import HttpResponse, Http404, HttpResponseRedirect
+from django.core.urlresolvers import reverse 
+from django.template import RequestContext, loader #allows it to load templates from blocbox/templates
+from core.models import UserInfo, Transaction, Connection
+#from django.contrib.auth.models import User #dont need this because not using User - maybe why it create table..
+from core.forms import UserForm, HostForm
+#Important the authentication and login functions -- not sure that i can use with custom model
+from django.contrib.auth import authenticate, login, get_user_model, logout
+from django.contrib.auth.decorators import login_required
+##Adding email functionality (http://catherinetenajeros.blogspot.com/2013/03/send-mail.html)
+from django.core.mail import send_mail
+from django.template.loader import render_to_string
+from django.views.generic.list import ListView
+#import scheduling stuff
+import datetime
+import pytz
+from urllib import quote
+#from schedule import periods
+from schedule.periods import Month
+from schedule.periods import weekday_names
+from schedule.conf.settings import GET_EVENTS_FUNC, OCCURRENCE_CANCEL_REDIRECT
+from schedule.forms import EventForm, OccurrenceForm
+from schedule.models import Calendar, Occurrence, Event
+from schedule.models.calendars import CalendarRelation
+from schedule.utils import check_event_permissions, coerce_date_dict
+from django.utils import timezone
 
-# Create your views here.
+
+#Write a custom template filter:
+from django.template.defaulttags import register
+@register.filter
+def get_item(dictionary, key):
+    return dictionary.get(key)
+
+    
+#jessstest - rendering calendar, note that claneder_slug is passed as argument in URL in base scheduling app
+def jesstest(request, calendar_slug_single = "testcalendar1", host_id=2):
+    enduser = request.user
+    host = get_object_or_404(UserInfo, pk=host_id)
+    connections_all = Connection.objects.filter(end_user=enduser) 
+    #I am copying the following date passing arguments from the calendar_by_periods function in schedule/views.py.. but i want to know how to just call that here   
+    #calndar stuff initially defined in schedule.views.calendar_by_periods    
+    #get the date -- coudl also do this with date = datetime.datetime.now()
+    try:
+        date = coerce_date_dict(request.GET)
+    except ValueError:
+        raise Http404
+    if date:
+        try:
+            date = datetime.datetime(**date)
+        except ValueError:
+            raise Http404
+    else:
+        date = timezone.now()
+        local_timezone = request.session.setdefault('django_timezone', 'UTC')     
+    local_timezone = pytz.timezone(local_timezone) #this is working]
+    thismonthname = Month(date, None, None, local_timezone) 
+    cal_list = Calendar.objects.all()
+    calendar_objects = {} 
+    event_list_objects = {}
+    thismonth_objects = {}
+    test_calslugs = []
+    for cal in cal_list:
+        event_list = GET_EVENTS_FUNC(request, cal)
+        calendar_objects[cal.slug] = get_object_or_404(Calendar, slug=cal.slug)
+        thismonth_objects[cal.slug] = Month(event_list, date, None, None, local_timezone)
+    #for a single calendar called i
+    calendar_single = get_object_or_404(Calendar, slug=calendar_slug_single) #this is working
+    event_list_single = GET_EVENTS_FUNC(request, calendar_single)  #this is working 
+    thismonth_object_single = Month(event_list_single, date, None, None, local_timezone) #specific to the calendar  
+    #Show all calendars associated with a particular host, host_id is currently defined above when called - want to pass it in URL
+    cal_relations_all = CalendarRelation.objects.all() #this is a list of CalendarRelation objects
+    cal_relations_host = CalendarRelation.objects.filter(object_id=host.id)
+    cal_relations_host_count = CalendarRelation.objects.filter(object_id=host.id).count()
+    cal_list_host = []
+    for cal in cal_relations_host:
+        cal_list_host.append(get_object_or_404(Calendar, id=cal.calendar_id))
+    return render(request, 'blocbox/jesstest.html', { 
+        'enduser':enduser, 
+        'host':host, 
+        'connections_all':connections_all,
+    	  'date':date, 
+    	  'thismonth_objects':thismonth_objects,
+    	  'thismonth_object_single':thismonth_object_single,
+    	  'thismonthname':thismonthname,
+    	  'weekday_names': weekday_names,
+        'cal_list':cal_list,
+        'calendar_objects':calendar_objects,
+    	  'calendar_single': calendar_single,
+    	  'cal_relations_all': cal_relations_all,
+    	  'cal_relations_host': cal_relations_host,
+    	  'cal_relations_host_count': cal_relations_host_count,
+    	  'cal_list_host': cal_list_host,
+    	  'here': quote(request.get_full_path())
+    }) 
+
+#bootsrap test - copy of the waitlist sign-up page
+def bootstraptest(request):
+    return render(request, 'blocbox/x_bootstraptest.html') 
+
+#style tests
+def styletest(request):
+    return render(request, 'blocbox/x_styletest.html') 
+    
+"""
+JMY NOTE ON DICTIONARIES VERSUS  LISTS:
+    Lists: 	a list of values. Each one of them is numbered, starting from zero - the first one is numbered zero, the second 1, the third 2, etc. 
+    				You can remove values from the list, and add new values to the end. Example: Your many cats' names.
+    				
+    				cats = ['Tom', 'Snappy', 'Kitty', 'Jessie', 'Chester']
+    				
+    Tuples: like lists but you can't change their values. 
+    				The values that you give it first up, are the values that you are stuck with for the rest of the program. 
+    				Again, each value is numbered starting from zero, for easy reference. Example: the names of the months of the year.
+    				
+    				months = ('January','February','March','April','May','June', 'July','August','September','October','November','  December')
+
+    Dictionaries: You have an 'index' of words, and for each of them a definition. In python, the word is called a 'key', and the definition a 'value'. The values in a dictionary aren't numbered - tare similar to what their name suggests - a dictionary. In a dictionary, you have an 'index' of words, and for each of them a definition. In python, the word is called a 'key', and the definition a 'value'. The values in a dictionary aren't numbered - they aren't in any specific order, either - the key does the same thing. You can add, remove, and modify the values in dictionaries. Example: telephone book.
+
+"""
+
+"""
+def denyrequestconnect(request, host_id, user_id):
+#url(r'^email/(?P<userinfo_id>\d+)/requestconnectdeny/(?P<userinfo_id>\d+)/$', views.denyrequestconnect, name='denyrequestconnect'),
+    context = RequestContext(request)
+    host = get_object_or_404(UserInfo, pk=host_id)
+    enduser = get_object_or_404(UserInfo, pk=user_id)
+"""
