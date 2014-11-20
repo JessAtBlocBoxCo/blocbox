284cd2557ce1d7e778100b38456bf4a235b4fd43
jess <jess@blocbox.co>
2014-11-19 19:26:44 -0800

    editing the epayment views

--- billing/urls.py
+++ billing/urls.py
@@ -1,5 +1,6 @@
 #This is blocbox/billing/urls.py
 from django.conf.urls import patterns, include, url
+from django.conf.urls.defaults import *
 from django.conf import settings
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.contrib import admin
