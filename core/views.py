3c16e3f08252f5ef052c6c10d5ec6a79e956c988
jess <jess@blocbox.co>
2014-11-22 13:15:58 -0800

    changing hostprofile url and search page to update

--- core/views.py
+++ core/views.py
@@ -300,13 +300,13 @@ def startashipment(request):
 #This is the REturn URL for paypal IPN so  eeds to be CSRF exempt
 @csrf_exempt		
 def shippackage(request, host_id=2): #passes the host_id argument in URL
-    context = RequestContext(request)
+    #context = RequestContext(request)
     enduser = request.user
     if host_id:
         host = get_object_or_404(UserInfo, pk=host_id)
     else:
     	  host = None
-    return render(request, 'blocbox/shippackage.html', {'enduser':enduser, 'host':host,}, context)
+    return render(request, 'blocbox/shippackage.html', {'enduser':enduser, 'host':host,} )
 
 
 
