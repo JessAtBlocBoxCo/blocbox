4b9fd8f8bdf7a0df5cdfe827e940c27b03e9ea20
jess <jess@blocbox.co>
2014-11-22 20:08:07 -0800

    editing the view in paypal.standard.ipn.views.py

--- paypal/standard/ipn/views.py
+++ paypal/standard/ipn/views.py
@@ -128,7 +128,7 @@ def ask_for_money(request, host_id=2, paymentoption="package"): #default amount 
         business = settings.PAYPAL_RECEIVER_EMAIL #want it to show as business name (Blocbox)
     returnmessage = "Return to Blocbox and Ship Your Package to " + host.first_name
     returnurl = "http://www.blocbox.co/shippackage/host" + str(host.id) +"/"
-    notifyurl = "http://www.blocbox.co/ipn/notify" + str(host.id) +"/"
+    notifyurl = "http://www.blocbox.co/payment/ipn/notify" + str(host.id) +"/"
     transcount = PayPalIPN.objects.filter(receiver_email=host.email).count() + 1 #counts transactions that this receiver_email has received (could change to host email) 
     invoice = "h" + str(host.id) + "u" + str(enduser.id) + "n" +str(transcount) #h2u14n13 = transaciton between host2, user14, host's 13th transaction
     local_timezone = request.session.setdefault('django_timezone', 'UTC') 
