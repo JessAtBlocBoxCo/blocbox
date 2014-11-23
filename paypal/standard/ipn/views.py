ac9677054a957bb0046a5cd76cf6c01b293a5f46
jess <jess@blocbox.co>
2014-11-22 16:24:54 -0800

    changing paypal image

--- paypal/standard/ipn/views.py
+++ paypal/standard/ipn/views.py
@@ -133,7 +133,7 @@ def ask_for_money(request, host_id=2, paymentoption="package"): #default amount 
         "item_name": youselected,
         "cbt": returnmessage, #Sets value for return to merchant button
         "image_url": "http://www.blocbox.co/static/blocbox/images/Logo-and-name---orange-drop2_paypal.png",
-        "invoice": host.email,
+        "invoice": invoice,
         "custom": host.email,
         #need keywords for that reverse
         "notify_url": "http://www.blocbox.co" + reverse('payment:paypal_ipn_notify'),
