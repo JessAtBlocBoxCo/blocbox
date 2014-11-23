011dcd486c941cedc5803ad47e4db89e04a45a69
jess <jess@blocbox.co>
2014-11-22 20:48:32 -0800

    editing the view in paypal.standard.ipn.views.py

--- paypal/standard/ipn/views.py
+++ paypal/standard/ipn/views.py
@@ -108,7 +108,6 @@ def ask_for_money(request, host_id=2, paymentoption="package"): #default amount 
         host = get_object_or_404(UserInfo, pk=host_id)
     else:
     	  host = None
-    date = timezone.now()
     if paymentoption=="bundle10":
         amount="15.00"
         youselected="Bundle of 10 Packages"
@@ -128,7 +127,12 @@ def ask_for_money(request, host_id=2, paymentoption="package"): #default amount 
         business = settings.PAYPAL_RECEIVER_EMAIL #want it to show as business name (Blocbox)
     returnmessage = "Return to Blocbox and Ship Your Package to " + host.first_name
     transcount = PayPalIPN.objects.filter(receiver_email=host.email).count() + 1 #counts transactions that this receiver_email has received (could change to host email) 
-    datenumber = datetime.datetime.now()
+    date = datetime.datetime.now()
+    year = date.year
+    month = date.month
+    day = date.day
+    hour = date.time.hour
+    datvars = str(year) + str(month) + str(day) + str(hour)
     invoice = "h" + str(host.id) + "u" + str(enduser.id) + "n" +str(transcount) +"d" #h2u14n13 = transaciton between host2, user14, host's 13th transaction
     local_timezone = request.session.setdefault('django_timezone', 'UTC') 
     #For a list of fields: https://developer.paypal.com/webapps/developer/docs/classic/paypal-payments-standard/integration-guide/Appx_websitestandard_htmlvariables/
@@ -151,7 +155,7 @@ def ask_for_money(request, host_id=2, paymentoption="package"): #default amount 
     #context = {"form": form}
     return render(request, 'blocbox/payment.html', {
 		    'enduser':enduser, 'host':host,
-    	  'date':date, 'local_timezone':local_timezone, 'datenumber':datenumber,
+    	  'date':date, 'local_timezone':local_timezone, 'datevars':datevars,
     	  'amount':amount, "youselected": youselected,
     	  'here': quote(request.get_full_path()), 'form': form,
     })
