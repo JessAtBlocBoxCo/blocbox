945592f3fb3a021f263b2d1d1f9d811e806e1aef
jess <jess@blocbox.co>
2014-12-09 13:20:02 -0800

    adding transactions back to list of installed apps

--- blocbox/settings.py
+++ blocbox/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'core',
-    #'transactions',
+    'transactions',
     'south',   
     'datasci',
     'password_reset',
