df5ce1dadc42303fab5b724604d761e3dd4e8ca3
jess <jess@blocbox.co>
2014-12-14 21:37:37 -0800

    adding forms

--- connections/admin.py
+++ connections/admin.py
@@ -3,7 +3,7 @@ from django.contrib import admin
 from django.contrib.auth.models import Group
 from django.contrib.auth.admin import UserAdmin
 from django.contrib.auth.forms import ReadOnlyPasswordHashField
-from connection.models import Connection
+from connections.models import Connection
 from django.contrib.auth.forms import (UserCreationForm, UserChangeForm,
     AdminPasswordChangeForm)
  
