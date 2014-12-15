db5f30e3993f697043021a3479ff3f6994f69c5f
jess <jess@blocbox.co>
2014-12-14 21:46:45 -0800

    Fixing glitch in backends.py

--- core/backends.py
+++ core/backends.py
@@ -1,6 +1,6 @@
 from django.contrib.auth.backends import ModelBackend
 #from django.contrib.admin.models import User
-from core.models import UserInfo, Connection
+from core.models import UserInfo
 
 class EmailAuthBackEnd(ModelBackend):
     def authenticate(self, email=None, password=None, **kwargs):
