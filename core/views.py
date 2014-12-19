c96bed105fb824128e6cb99880e14b1d73b3b73c
jess <jess@blocbox.co>
2014-12-19 15:58:24 -0800

    adding content tag to site_base for schedule

--- core/views.py
+++ core/views.py
@@ -389,6 +389,9 @@ def startashipment(request, host_id=None, dayrangestart=None, dayrangeend=None, 
         AvailabilityCal = get_object_or_404(Calendar, slug=AvailabilityCal_Slug)
         AvailabilityCal_EventList = GET_EVENTS_FUNC(request, AvailabilityCal)
         AvailabilityCal_MonthObject = Month(AvailabilityCal_EventList, date, None, None, local_timezone)
+    else:
+        AvailabilityCal = None
+        AvailabilityCal_MonthObject = None
     return render(request, 'blocbox/startashipment.html', {
 		    'enduser':enduser, 'host': host, 'connections_all': connections_all, 
 		    'dayrangestart': dayrangestart, 'dayrangeend': dayrangeend, 'date':date, 
