5ebf6469f79be27fc10ca7af8c69473f806e3494
jess <jess@blocbox.co>
2014-12-19 22:35:11 -0800

    editing dates

--- schedule/templatetags/scheduletags.py
+++ schedule/templatetags/scheduletags.py
@@ -42,7 +42,7 @@ def day_cell(context, calendar, day, month, size="regular"):
 
 
 @register.inclusion_tag("schedule/_daily_table.html", takes_context=True)
-def daily_table(context, day, width, width_slot, height, start=8, end=20, increment=30):
+def daily_table(context, day, width, width_slot, height, start, end, increment=30):
     """
       Display a nice table with occurrences and action buttons.
       Arguments:
