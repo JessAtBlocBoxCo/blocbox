411703cdc7a8102577d781a233720f27087f82f7
jess <jess@blocbox.co>
2014-12-20 13:33:34 -0800

    tell user they have no conflicts if they dont

--- core/views.py
+++ core/views.py
@@ -422,7 +422,7 @@ def startafavor(request, host_id=None):
 		return render(request, 'blocbox/startafavor.html', {'enduser':enduser,})
 
 #starat a shipmetn view if requested from navbar
-def nav_startafavor(request):
+def nav_startafavor(request, host=None):
     enduser = request.user
     connections_all = Connection.objects.filter(end_user=enduser) 
     connections_count = connections_all.count() #count them,removing status=0 after host_user=host
