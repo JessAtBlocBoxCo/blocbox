d3670a5f4079c6550309bd4c5471a5bf848eb8c9
jess <jess@blocbox.co>
2014-12-19 19:38:07 -0800

    new

--- schedule/views.py
+++ schedule/views.py
@@ -2,7 +2,7 @@ import json
 import pytz
 import datetime
 from urllib import quote
-
+from django.conf import settings
 from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render_to_response, get_object_or_404
