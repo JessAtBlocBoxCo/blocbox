d64d4a6a3c69e5d920392b17c5a181f981d86d6a
jess <jess@blocbox.co>
2014-12-20 16:40:46 -0800

    adding fields to transactinos for expected delivery date

--- paypal/standard/ipn/views.py
+++ paypal/standard/ipn/views.py
@@ -164,7 +164,7 @@ def ask_for_money(request, host_id=2, favortype="package", paymentoption="perpac
     }    
     form = PayPalPaymentsForm(initial=paypal_dict) #in paypal/standard/forms.py
     #Update transaction table
-    datenow = datetime.datetime.today
+    datenow = datetime.datetime.today()
     trans = Transaction() 
     if host_id:
         trans.host = host
