fe6c5e5c252fba006d90e5143b2921b1f554dd4d
Jess <jess@blocbox.co>
2015-01-10 21:37:37 -0800

    transaction admin pane

--- transactions/admin.py
+++ transactions/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from transactions.models import Transaction
 
 class TransactionAdmin(admin.ModelAdmin): 
-    list_display = ('id', 'enduser', 'host', 'favortype', 'date_requested', 'status', 'tracking', 'price', 'deliverydatenotracking_rangeend', 'date_requested_notime')
+    list_display = ('id', 'enduser', 'host', 'favortype', 'date_requested', 'status', 'tracking', 'price', 'deliverydatenotracking_rangeend')
     list_filter = ['host', 'enduser', 'favortype']
     search_fields = ['host', 'enduser']  
 
