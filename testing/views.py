de99c64e2a1694378630ec286ae19e8bd28b9142
Jess <jess@blocbox.co>
2015-01-11 23:20:43 -0800

    adding aftership api

--- testing/views.py
+++ testing/views.py
@@ -119,8 +119,8 @@ def jesscaltest(request, host_id=None): # calendar_slug_single = "testcalendar1"
         cal_relations_host_count = None
     #test the API
     AFTERSHIP_API_KEY = settings.AFTERSHIP_API_KEY #DEFINED IN SETTINGS.PY
-    api = aftership.APIv4('488caf4b-e7aa-4634-928b-2df5de94af9f')
-    #801e84c7-bae1-4afb-b294-51ca02a63d02
+    #api = aftership.APIv4('488caf4b-e7aa-4634-928b-2df5de94af9f')
+    api = aftership.APIv4('801e84c7-bae1-4afb-b294-51ca02a63d02')
     #api = aftership.APIv4('AFTERSHIP_API_KEY') #Defined in settings.py
     couriers = api.couriers.all.get()
     return render(request, 'testing/jesstest.html', { 
