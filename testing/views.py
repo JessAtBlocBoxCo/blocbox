4a40c0b145333ed3407732f88c48cd7380aefb26
Jess <jess@blocbox.co>
2015-01-12 13:34:39 -0800

    testing api

--- testing/views.py
+++ testing/views.py
@@ -148,7 +148,6 @@ def jesscaltest(request, host_id=None): # calendar_slug_single = "testcalendar1"
     track_tag = api.trackings.get(slug_get_tracking, number_get_tracking, fields=['tag'])
     track_order_id = api.trackings.get(slug_get_tracking, number_get_tracking, fields=['order_id'])
     origin_country_iso3 = api.trackings.get(slug_get_tracking, number_get_tracking, fields=['origin_country_iso3'])
-    track_slug = api.tracking.get(slug_get_tracking, number_get_tracking, fields=['slug'])
     tracking_account_number = api.tracking.get(slug_get_tracking, number_get_tracking, fields = ['tracking_account_number'])
     track_source = api.tracking.get(slug_get_tracking, number_get_tracking, fields = ['source'])
     tracked_count = api.tracking.get(slug_get_tracking, number_get_tracking, fields = ['tracked_count'])
@@ -159,6 +158,7 @@ def jesscaltest(request, host_id=None): # calendar_slug_single = "testcalendar1"
     tracking_ship_date = None #api.tracking.get(slug_get_tracking, number_get_tracking, fields=['tracking_ship_date'])
     track_customeer = None #api.tracking.get(slug_get_tracking, number_get_tracking, fields=['customer_name'])
     track_shipment_type = None #api.tracking.get(slug_get_tracking, number_get_tracking, fields=['shipment_type'])
+    track_slug = None #api.tracking.get(slug_get_tracking, number_get_tracking, fields=['slug'])
     #To see all tracking fields print the variable track_allfields
     #Tracking fields from website
     #tracking fields: created_at, updated_at, tracking_number, slug, active, custom_fields (tuple), custom_name, customer_name, destination_country, emails (list?), expected_delivery
