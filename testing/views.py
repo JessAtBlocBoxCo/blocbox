c534096415a029187cb5955a3416a78d92c8718b
Jess <jess@blocbox.co>
2015-01-12 10:23:20 -0800

    removing created at

--- testing/views.py
+++ testing/views.py
@@ -137,11 +137,13 @@ def jesscaltest(request, host_id=None): # calendar_slug_single = "testcalendar1"
 		# create tracking: https://www.aftership.com/docs/api/4/trackings/post-trackings
     #api.trackings.post(tracking=dict(slug=slug_to_track, tracking_number=number_to_track, title="Test Title for Create Tracking")) 
     # get tracking by slug and number, return 'title' and 'created_at' field: https://www.aftership.com/docs/api/4/trackings/get-trackings-slug-tracking_number
+    #Notice that you can always use/:id to replace /:slug/:tracking_number -- .g. DELETE /trackings/:id
     # all of the fields: 
+    track_info = api.trackings.get(slug_get_tracking, number_get_tracking, fields=['title', 'created_at'])
     track_tracking_number = api.trackings.get(slug_get_tracking, number_get_tracking, fields=['tracking_number'])
     track_tracking_number_single = api.trackings.get(slug_get_tracking, number_get_tracking, field='tracking_number')
     track_title = api.trackings.get(slug_get_tracking, number_get_tracking, fields=['title'])
-    # track_created_at = api.tracking.get(slug_get_tracking, number_get_tracking, fields=['created_at'])
+    """track_created_at = api.tracking.get(slug_get_tracking, number_get_tracking, fields=['created_at'])
     track_updated_at = api.tracking.get(slug_get_tracking, number_get_tracking, fields=['updated_at'])
     track_slug = api.tracking.get(slug_get_tracking, number_get_tracking, fields=['slug'])
     track_active = api.tracking.get(slug_get_tracking, number_get_tracking, fields=['active'])
@@ -161,7 +163,7 @@ def jesscaltest(request, host_id=None): # calendar_slug_single = "testcalendar1"
     track_tag = api.tracking.get(slug_get_tracking, number_get_tracking, fields=['tag'])
     track_tracked_count = api.tracking.get(slug_get_tracking, number_get_tracking, fields=['tracked_count'])
     track_unique_token = api.tracking.get(slug_get_tracking, number_get_tracking, fields=['unique_token'])
-    track_checkpoints = api.tracking.get(slug_get_tracking, number_get_tracking, fields=['checkpoints']).all
+    track_checkpoints = api.tracking.get(slug_get_tracking, number_get_tracking, fields=['checkpoints']).all """
     
     #tracking fields: created_at, updated_at, tracking_number, slug, active, custom_fields (tuple), custom_name, customer_name, destination_country, emails (list?), expected_delivery
 		#order_id, origin_country_iso3, shipment_package_count, shipment_type, signed_by, smses, source, tag, title, tracked_count, unique_token, checkpoints (list with sub variables)
@@ -183,7 +185,7 @@ def jesscaltest(request, host_id=None): # calendar_slug_single = "testcalendar1"
     	  'here': quote(request.get_full_path()),
     	  'transactions_all': transactions_all, 'shipments_all': shipments_all, 'otherfavors_all': otherfavors_all,
  				'aftership_api_key':AFTERSHIP_API_KEY, 'couriers': couriers, 
- 				'track_tracking_number': track_tracking_number, 'track_tracking_number_single': track_tracking_number_single,
+ 				'track_info': track_info, 'track_tracking_number': track_tracking_number, 'track_tracking_number_single': track_tracking_number_single,
  				#'track_title': track_title, 'track_created_at': track_created_at, 'track_updated_at': track_updated_at, '
     }) 
 
