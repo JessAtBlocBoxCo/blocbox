f1fddaac060352458b44ca809572f962bfb59e00
Jess <jess@blocbox.co>
2015-01-12 13:38:47 -0800

    testing api

--- testing/views.py
+++ testing/views.py
@@ -148,7 +148,6 @@ def jesscaltest(request, host_id=None): # calendar_slug_single = "testcalendar1"
     track_tag = api.trackings.get(slug_get_tracking, number_get_tracking, fields=['tag'])
     track_order_id = api.trackings.get(slug_get_tracking, number_get_tracking, fields=['order_id'])
     origin_country_iso3 = api.trackings.get(slug_get_tracking, number_get_tracking, fields=['origin_country_iso3'])
-    tracked_count = api.tracking.get(slug_get_tracking, number_get_tracking, fields = ['tracked_count'])
     track_note = api.tracking.get(slug_get_tracking, number_get_tracking, fields = ['note'])
     ##CANNOT ACCESS THE FOLLOWING FIELDS -- WHY NOTE?
     track_created_at = None #api.tracking.get(slug_get_tracking, number_get_tracking, fields=['created_at'])
@@ -159,6 +158,7 @@ def jesscaltest(request, host_id=None): # calendar_slug_single = "testcalendar1"
     track_slug = None #api.tracking.get(slug_get_tracking, number_get_tracking, fields=['slug'])
     tracking_account_number = None #api.tracking.get(slug_get_tracking, number_get_tracking, fields = ['tracking_account_number'])
     track_source = None #api.tracking.get(slug_get_tracking, number_get_tracking, fields = ['source'])
+    tracked_count = None #api.tracking.get(slug_get_tracking, number_get_tracking, fields = ['tracked_count'])
     #To see all tracking fields print the variable track_allfields
     #Tracking fields from website
     #tracking fields: created_at, updated_at, tracking_number, slug, active, custom_fields (tuple), custom_name, customer_name, destination_country, emails (list?), expected_delivery
