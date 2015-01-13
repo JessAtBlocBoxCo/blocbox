85da5787c8961beebcef0423f5803d062fd938d1
Jess <jess@blocbox.co>
2015-01-13 11:37:21 -0800

    adding funciton to tracking widget to add tracking number to aftership

--- core/views.py
+++ core/views.py
@@ -113,7 +113,7 @@ def dashboard(request, host_id=None, trans=None, track_id=None, confirm_id=None,
     						    title="Shipment " + trans.id+": User "+enduser.email+" to Host "+trans.host.email, 
     						    order_id=str(trans.id),
     						    customer_name = trans.enduser.email,
-    						    emails=[trans.enduser.email, trans.host.email}], #Emails for notifications
+    						    emails=[trans.enduser.email, trans.host.email], #Emails for notifications
     						    custom_fields=dict(Host_Email={{trans.host.email}}, Invoice={{trans.invoice}})
     						    #Eventually consider add SMSEs here to add phone notifications - its 4 cents per SMS so may not be worth it
     						    )) 	 
