833db8890406c26b95a136f906a4c029e56aeb75
Jess <jess@blocbox.co>
2015-01-13 13:51:54 -0800

    add a title field to the transactions table

--- transactions/models.py
+++ transactions/models.py
@@ -19,7 +19,7 @@ class Transaction(models.Model):
     #host = models.CharField("Host/Provider", max_length=100, blank=True, null=True)
     #enduser = models.CharField("EndUser/Receiver", max_length=100, blank=True, null=True)
     price = models.DecimalField('Amount Paid', max_digits=6, decimal_places=2, blank=True, null=True)
-    title = model.CharField("User Provided Title", max_length=100, blank=True, null=True)   
+    title = models.CharField("User Provided Title", max_length=100, blank=True, null=True)   
     date_requested = models.DateTimeField(default=datetime.datetime.today)
     date_requested_notime = models.DateField(default = datetime.date.today, null=True)
     favortype = models.CharField("Favor Type", max_length=100, blank=True, null=True)
