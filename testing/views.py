9dbfc07472249a6b5a17a736638e31b29a47597b
Jess <jess@blocbox.co>
2015-01-12 17:47:16 -0800

    testing api

--- testing/views.py
+++ testing/views.py
@@ -160,7 +160,7 @@ def jesscaltest(request, host_id=None): # calendar_slug_single = "testcalendar1"
     	  if shipment.tracking == None:
     	  	  courier_slugs[shipment.id] = None
     	  else:
-            tracking_no = shipment.tracking
+            tracking_no = str(shipment.tracking) #the str function removes the preceding u'
             tracking_numbers[shipment.id] = tracking_no
             #courier_allfields = api.couriers.detect.post(tracking=dict(tracking_number=tracking_no))
             #courier_list = courier_allfields.get(u'couriers')
