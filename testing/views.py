820b2d0a8044457aa81ebbea168baf2947048723
Jess <jess@blocbox.co>
2015-01-12 19:26:59 -0800

    adding list of couriers

--- testing/views.py
+++ testing/views.py
@@ -158,7 +158,7 @@ def jesscaltest(request, host_id=None): # calendar_slug_single = "testcalendar1"
     shipment_withtracking_id = []
     for shipment in shipments_all:
     	  if shipment.tracking:
-    	  	  shipment_withtracking_id.append(shipment.id)
+            shipment_withtracking_id.append(shipment.id)
             tracking_no = str(shipment.tracking) #the str function removes the preceding u'
             courier_allfields = api.couriers.detect.post(tracking=dict(tracking_number=tracking_no))
             courier_list = courier_allfields.get(u'couriers')
