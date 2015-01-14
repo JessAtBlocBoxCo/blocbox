2bb172f7303ed51e235914ee05f2264696bfa60b
Jess <jess@blocbox.co>
2015-01-14 15:52:58 -0800

    dashboard just paid transactions

--- core/views.py
+++ core/views.py
@@ -208,6 +208,7 @@ def dashboard(request, host_id=None, trans=None, track_id=None, confirm_id=None,
         shipment_tuple['title']=shipment.title
         shipment_tuple['note_to_host']=shipment.note_to_host
         shipment_tuple['on_aftership']=shipment.on_aftership
+        shipment_tuple['date_completed']=shipment.date_completed
         shipment_tuple['aftership']={}  
         if shipment.on_aftership: 
             #populate the aftership_tracking sub-tuble                 
