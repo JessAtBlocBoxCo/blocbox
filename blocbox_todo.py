10c21b360d91960032b2f516919ab302f6129ce2
Jess <jess@blocbox.co>
2015-01-14 17:38:40 -0800

    dashboard just paid transactions

--- blocbox_todo.py
+++ blocbox_todo.py
@@ -1,4 +1,6 @@
 """jess dashboard to do"""
+1. add expected delivery
+2. separte those en route from those waiting to be picked up - put waiting to be picked up first
 - edit the note to the host when shipment is sent to include the note to host 
 - update the tracking widget - dont use the aftership one - crate my own so that it doesnt take so mjuch time to load an dlook like shit
 - make the payment_processed indicator work
