fb621efcb71c1ee7445f639236d26bb61f067ff0
Jess <jess@blocbox.co>
2015-01-15 17:25:47 -0800

    cleaning up dashboard in transit section

--- transactions/admin.py
+++ transactions/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from transactions.models import Transaction
 
 class TransactionAdmin(admin.ModelAdmin): 
-    list_display = ('id', 'payment_processed', 'trans_complete', 'on_aftership', 'enduser', 'host', 'title', 'favortype', 'date_requested', 'dayrangeend', 'tracking', 'price', 'enduser_rating')
+    list_display = ('id', 'payment_processed', 'trans_complete', 'on_aftership', 'enduser', 'host', 'title', 'favortype', 'date_requested', 'tracking', 'invoice', 'price', 'enduser_rating',)
     list_filter = ['host', 'enduser', 'favortype']
     search_fields = ['host', 'enduser']  
 
