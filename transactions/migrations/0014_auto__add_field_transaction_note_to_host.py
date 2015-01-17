3f45cf584ae59fff793992db9136feb33105ad5c
Jess <jess@blocbox.co>
2015-01-17 01:32:34 -0500

    new calendar app

---
+++ transactions/migrations/0014_auto__add_field_transaction_note_to_host.py
@@ -0,0 +1,116 @@
+# -*- coding: utf-8 -*-
+from south.utils import datetime_utils as datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models
+
+
+class Migration(SchemaMigration):
+
+    def forwards(self, orm):
+        # Adding field 'Transaction.note_to_host'
+        db.add_column(u'transactions_transaction', 'note_to_host',
+                      self.gf('django.db.models.fields.CharField')(max_length=200, null=True, blank=True),
+                      keep_default=False)
+
+
+    def backwards(self, orm):
+        # Deleting field 'Transaction.note_to_host'
+        db.delete_column(u'transactions_transaction', 'note_to_host')
+
+
+    models = {
+        u'core.userinfo': {
+            'FBlink': ('django.db.models.fields.URLField', [], {'max_length': '200', 'blank': 'True'}),
+            'Meta': {'object_name': 'UserInfo'},
+            'about_me': ('django.db.models.fields.CharField', [], {'max_length': '300', 'blank': 'True'}),
+            'address_approx': ('django.db.models.fields.CharField', [], {'max_length': '100', 'null': 'True', 'blank': 'True'}),
+            'address_latitude': ('django.db.models.fields.DecimalField', [], {'null': 'True', 'max_digits': '9', 'decimal_places': '6', 'blank': 'True'}),
+            'address_longitude': ('django.db.models.fields.DecimalField', [], {'null': 'True', 'max_digits': '10', 'decimal_places': '6', 'blank': 'True'}),
+            'availability': ('django.db.models.fields.CharField', [], {'max_length': '250', 'blank': 'True'}),
+            'city': ('django.db.models.fields.CharField', [], {'max_length': '30', 'blank': 'True'}),
+            'customchar': ('django.db.models.fields.CharField', [], {'max_length': '200', 'null': 'True', 'blank': 'True'}),
+            'customcharthree': ('django.db.models.fields.CharField', [], {'max_length': '100', 'null': 'True', 'blank': 'True'}),
+            'customchartwo': ('django.db.models.fields.CharField', [], {'max_length': '200', 'null': 'True', 'blank': 'True'}),
+            'date_joined': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now', 'null': 'True', 'blank': 'True'}),
+            'email': ('django.db.models.fields.EmailField', [], {'unique': 'True', 'max_length': '254'}),
+            'favorscompleted': ('django.db.models.fields.IntegerField', [], {'null': 'True', 'blank': 'True'}),
+            'favorsrequested': ('django.db.models.fields.IntegerField', [], {'null': 'True', 'blank': 'True'}),
+            'first_name': ('django.db.models.fields.CharField', [], {'max_length': '100', 'blank': 'True'}),
+            'host': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'host_aboutme': ('django.db.models.fields.CharField', [], {'max_length': '350', 'blank': 'True'}),
+            'hostinterest': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'hostrating': ('django.db.models.fields.DecimalField', [], {'null': 'True', 'max_digits': '3', 'decimal_places': '2', 'blank': 'True'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'imageurl': ('django.db.models.fields.URLField', [], {'max_length': '200', 'blank': 'True'}),
+            'intro_message': ('django.db.models.fields.CharField', [], {'max_length': '350', 'blank': 'True'}),
+            'is_active': ('django.db.models.fields.BooleanField', [], {'default': 'True'}),
+            'is_admin': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'last_login': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
+            'last_name': ('django.db.models.fields.CharField', [], {'max_length': '100', 'blank': 'True'}),
+            'need_autocare': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'need_carsharing': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'need_childcare': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'need_housemaint': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'need_housesitting': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'need_laundry': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'need_lawn': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'need_letin': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'need_other': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'need_petcare': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'need_plantcare': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'need_rentals': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'need_storage': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'neighborhood': ('django.db.models.fields.CharField', [], {'max_length': '100', 'null': 'True', 'blank': 'True'}),
+            'password': ('django.db.models.fields.CharField', [], {'max_length': '128'}),
+            'phone_number': ('django.db.models.fields.CharField', [], {'max_length': '12', 'null': 'True', 'blank': 'True'}),
+            'pickup_time': ('django.db.models.fields.CharField', [], {'max_length': '150', 'blank': 'True'}),
+            'price_package_annual': ('django.db.models.fields.DecimalField', [], {'null': 'True', 'max_digits': '5', 'decimal_places': '2', 'blank': 'True'}),
+            'price_package_bundle10': ('django.db.models.fields.DecimalField', [], {'null': 'True', 'max_digits': '4', 'decimal_places': '2', 'blank': 'True'}),
+            'price_package_month20': ('django.db.models.fields.DecimalField', [], {'null': 'True', 'max_digits': '4', 'decimal_places': '2', 'blank': 'True'}),
+            'price_package_per': ('django.db.models.fields.DecimalField', [], {'null': 'True', 'max_digits': '4', 'decimal_places': '2', 'blank': 'True'}),
+            'services_offered': ('django.db.models.fields.CharField', [], {'max_length': '250', 'blank': 'True'}),
+            'st_address1': ('django.db.models.fields.CharField', [], {'max_length': '70', 'blank': 'True'}),
+            'st_address2': ('django.db.models.fields.CharField', [], {'max_length': '70', 'blank': 'True'}),
+            'state': ('django.db.models.fields.CharField', [], {'max_length': '2', 'blank': 'True'}),
+            'userrating': ('django.db.models.fields.DecimalField', [], {'null': 'True', 'max_digits': '3', 'decimal_places': '2', 'blank': 'True'}),
+            'whenimhome_days': ('django.db.models.fields.CharField', [], {'max_length': '250', 'blank': 'True'}),
+            'whenimhome_hours': ('django.db.models.fields.CharField', [], {'max_length': '250', 'blank': 'True'}),
+            'zipcode': ('django.db.models.fields.CharField', [], {'max_length': '5'})
+        },
+        u'transactions.transaction': {
+            'Meta': {'object_name': 'Transaction'},
+            'date_requested': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime(2015, 1, 14, 0, 0)'}),
+            'date_requested_notime': ('django.db.models.fields.DateField', [], {'default': 'datetime.date.today', 'null': 'True'}),
+            'dayrangeend': ('django.db.models.fields.IntegerField', [], {'null': 'True', 'blank': 'True'}),
+            'dayrangestart': ('django.db.models.fields.IntegerField', [], {'null': 'True', 'blank': 'True'}),
+            'deliverydate_tracking': ('django.db.models.fields.DateField', [], {'null': 'True', 'blank': 'True'}),
+            'deliverydatenotracking_rangeend': ('django.db.models.fields.DateField', [], {'null': 'True', 'blank': 'True'}),
+            'deliverydatenotracking_rangestart': ('django.db.models.fields.DateField', [], {'null': 'True', 'blank': 'True'}),
+            'enduser': ('django.db.models.fields.related.ForeignKey', [], {'blank': 'True', 'related_name': "'transaction_enduser'", 'null': 'True', 'to': u"orm['core.UserInfo']"}),
+            'enduser_comments': ('django.db.models.fields.CharField', [], {'max_length': '200', 'null': 'True', 'blank': 'True'}),
+            'enduser_issue': ('django.db.models.fields.CharField', [], {'max_length': '300', 'null': 'True', 'blank': 'True'}),
+            'enduser_rating': ('django.db.models.fields.DecimalField', [], {'null': 'True', 'max_digits': '2', 'decimal_places': '1', 'blank': 'True'}),
+            'favorstatus': ('django.db.models.fields.CharField', [], {'max_length': '50', 'null': 'True', 'blank': 'True'}),
+            'favortype': ('django.db.models.fields.CharField', [], {'max_length': '100', 'null': 'True', 'blank': 'True'}),
+            'host': ('django.db.models.fields.related.ForeignKey', [], {'blank': 'True', 'related_name': "'transaction_host'", 'null': 'True', 'to': u"orm['core.UserInfo']"}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'invoice': ('django.db.models.fields.CharField', [], {'max_length': '100', 'null': 'True', 'blank': 'True'}),
+            'note_to_host': ('django.db.models.fields.CharField', [], {'max_length': '200', 'null': 'True', 'blank': 'True'}),
+            'on_aftership': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'payment_option': ('django.db.models.fields.CharField', [], {'max_length': '30', 'null': 'True', 'blank': 'True'}),
+            'payment_processed': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'paypal_quantity': ('django.db.models.fields.IntegerField', [], {'null': 'True', 'blank': 'True'}),
+            'price': ('django.db.models.fields.DecimalField', [], {'null': 'True', 'max_digits': '6', 'decimal_places': '2', 'blank': 'True'}),
+            'shipment_courier': ('django.db.models.fields.CharField', [], {'max_length': '20', 'null': 'True', 'blank': 'True'}),
+            'status': ('django.db.models.fields.CharField', [], {'max_length': '50', 'null': 'True', 'blank': 'True'}),
+            'testfieldagain': ('django.db.models.fields.CharField', [], {'max_length': '50', 'null': 'True', 'blank': 'True'}),
+            'title': ('django.db.models.fields.CharField', [], {'max_length': '100', 'null': 'True', 'blank': 'True'}),
+            'tracking': ('django.db.models.fields.CharField', [], {'max_length': '50', 'null': 'True', 'blank': 'True'}),
+            'tracking_info_tuple_confirmd': ('django.db.models.fields.CharField', [], {'max_length': '2000', 'null': 'True', 'blank': 'True'}),
+            'trans_complete': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'youselected': ('django.db.models.fields.CharField', [], {'max_length': '50', 'null': 'True', 'blank': 'True'})
+        }
+    }
+
+    complete_apps = ['transactions']
