a2505417821ea30c97e708455cb1e8350e667de0
Jess <jess@blocbox.co>
2015-01-16 17:40:11 -0800

    more efficient dashboard view

--- schedule/admin.py
+++ schedule/admin.py
@@ -3,7 +3,8 @@ from django.contrib import admin
 from schedule.models import Calendar, Event, CalendarRelation, Rule
 
 class CalendarAdminOptions(admin.ModelAdmin):
-    prepopulated_fields = {"id", "slug": ("name",)}
+	  list_display = ('id', 'slug',)
+    prepopulated_fields = {"slug": ("name",)}
     search_fields = ['name']
 
 
