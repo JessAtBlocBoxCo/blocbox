99edd93507499cc9d641a0f1e61a0f5457b21725
Jess <jess@blocbox.co>
2015-01-17 13:39:55 -0800

    test cal

--- testing/views.py
+++ testing/views.py
@@ -104,6 +104,7 @@ def homebrew_cal(request):
                 conflict_day = conflict.date_from.day + day + 1 #range starts at zero so have to add 1
                 days_withconflicts_thismonth.append(conflict_day)
         #remove duplciates - hopefully they dont exist but the might
+        days_withconflicts_thismonth = []
         days_withconflicts_thismonth = list(set(days_withconflicts_thismonth))
         start_month = conflict.date_from.month #date_from.month, this is an integer
         if conflict.date_to:
