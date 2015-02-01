4118cef0530716ca74ab6d2dc774e941ecfe3f29
Jess <jess@blocbox.co>
2015-02-01 18:47:52 -0500

    moving paymetn stuff to transactions app

--- transactions/views.py
+++ transactions/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render
 import datetime
 import pytz
 from urllib import quote
+from django.views.decorators.csrf import csrf_exempt
 #from django
 from django.shortcuts import render, get_object_or_404, render_to_response
 from django.http import HttpResponse, Http404, HttpResponseRedirect
