6deab1440e2d4591ecc7bcaf06621bffa95507c4
Jess <jess@blocbox.co>
2015-02-02 11:23:41 -0500

    troubleshooting packagedays issue on startashipment

--- transactions/views.py
+++ transactions/views.py
@@ -36,7 +36,7 @@ from transactions.forms import CreatePackageTransaction
 
 #The Start a shipment process
 #We may want to move all of this stuff into the Transactions app
-def startashipment(request, host_id=None, calendar_slug_single = "testcalendar1",):
+def startashipment(request, host_id=None, calendar_slug_single = "testcalendar1", transaction_form_submitted=None):
     enduser = request.user
     if host_id:
         host = get_object_or_404(UserInfo, pk=host_id)
