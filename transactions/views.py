e4edbb29fe42896c17a9b2a1aea2ba9e169541a2
Jess <jess@blocbox.co>
2015-02-02 11:22:24 -0500

    troubleshooting packagedays issue on startashipment

--- transactions/views.py
+++ transactions/views.py
@@ -156,12 +156,12 @@ def startashipment(request, host_id=None, calendar_slug_single = "testcalendar1"
         host_package_conflict = False  
     #do payment variables/ transaction form stuff once they've checked the calendar day
     favortype='package'
-    transaction_form_submitted = False
+    #transaction_form_submitted = False
     packagedays_count = None
     trans_form_package = None
     invoice = None  
     #if the transaction form has been submitted redirect to new page
-    if transaction_form_submitted == True:
+    if transaction_form_submitted:
         return HttpResponseRedirect("/transactions/payment/host" + str(host.id) + "/invoice" + str(invoice) + "/favortype" + str(favortype) + "/") 
         cal_form = None   
     #if the transaction form has not been submitted  
