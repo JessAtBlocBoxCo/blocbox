65f7f01a61477c9f1aa60afbcd1e7ada2e2d9993
Jess <jess@blocbox.co>
2015-02-03 11:39:37 -0500

    working checkboxes

--- transactions/views.py
+++ transactions/views.py
@@ -92,7 +92,7 @@ def startashipment(request, host_id=None, transaction_form_submitted=False, invo
     #if host/no host - get caklendar_homebrew created fields
     if host:
         transcount = Transaction.objects.filter(host=host).count() + 1 #counts transactions that this receiver_email has received (could change to host email)
-        invoice = "H" + str(host.id) + "U" + str(enduser.id) + "N" +str(transcount) +"D" + str(date_today.month) + str(date_today.day) + str(time.hour) #h2u14n13d112210 = transaciton between host2, user14, host's 13th transaction
+        invoice = "H" + str(host.id) + "U" + str(enduser.id) + "N" +str(transcount) +"D" + str(date_today.month) + str(date_today.day) + str(time.hour) + "R" + str(random3digits) #h2u14N13D112210R123 = transaciton between host2, user14, host's 13th transaction
         conflicts = HostConflicts.objects.filter(host=host)
         for conflict in conflicts:  
             start_month = conflict.date_from.month #date_from.month, this is an integer
