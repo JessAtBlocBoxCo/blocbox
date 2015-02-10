8f7c408fef421dda40dd10bd5a2fae113e1abbcf
JB <john@blocbox.co>
2015-02-10 16:02:49 -0500

    jb

--- blocbox_todo.py
+++ blocbox_todo.py
@@ -107,15 +107,13 @@ JESS: other favors - add two use cases (1) wifi sharing; (2) community dinners/f
         
 "JOHN TO-DO"
 
+1. JOHN: shipment - Legend for the calendar
 
 2.    
 JOHN: landing - there should be a direct tlink to signing up as a host on the landing page. i think it shoudl say "esarch for a host in your neighborhood to get started" (less verbose though) and then a separate button saying "sign up to be a host"
 
 4.    
-JOHN: dashboard - Styling boxes
-JOHN: dashboard - Style fonts
 JOHN: dashboard - Placeholders or hide boxes when now shipments in process
-JOHN: dashboard - Legend for the calendar
 JOHN: dashbaord - the profile pictures ar enow upload-able and linked to the folder /home/django/blocbox/blocbox/static/user_uploads
 this is true on the profile and dashboard page on profle - you need to appply the sahe "dashpic" and "Dashcoainter" type of styles you did to the dashboard if you want it to look better
 JOHN: dashbaord - style the profile pic - so the user uplaodssimplepic, and then converted to glossy bubble
