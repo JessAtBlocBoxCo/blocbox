5a0f025fc64c6a23fd9a73ffe68ff314c48f66e0
Jess <jess@blocbox.co>
2015-02-15 11:57:59 -0800

    balance process

--- transactions/urls.py
+++ transactions/urls.py
@@ -27,7 +27,7 @@ urlpatterns = patterns('',
 		url(r'^shippackage/host(?P<host_id>\d+)/account_balance/trans(?P<trans_id>\d+)/$', 'transactions.views.shippackage_accountbalance', name='shippackage_accountbalance'),
 		#SHIPMENTthe blocbox.paypal.standard.ipn.views.ask_for_money view triggered by a SHIPMENT - with dayrangestart, danrange end
     url(r'^payment/host(?P<host_id>\d+)/invoice(?P<invoice>\w+)/favortype(?P<favortype>\w+)/$', 'paypal.standard.ipn.views.ask_for_money', name='ipn_ask_shipment'),
-    url(r'^payment/host(?P<host_id>\d+)/invoice(?P<invoice>\w+)/favortype(?P<favortype>\w+)/balance(?P<balance>\w+)/$', 
+    url(r'^payment/host(?P<host_id>\d+)/invoice(?P<invoice>\w+)/favortype(?P<favortype>\w+)/balance(?P<balance>\w+)/$', 'paypal.standard.ipn.views.ask_for_money', name='ipn_ask_shipment_balance'),
 		#FAVORthe blocbox.paypal.standard.ipn.views.ask_for_money view triggered by a FAVOR (e.g., no dayrangestart or dayrange end)
 		url(r'^payment/host(?P<host_id>\d+)/ipn/(?P<favortype>\w+)/(?P<paymentoption>\w+)/$', 'paypal.standard.ipn.views.ask_for_money', name='ipn_ask_favor'),
 		#Notify URL for paypal IPAN
