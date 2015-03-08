fc126cc5da9c57d46ed644f04bf0419181b02ec4
Jess <jess@blocbox.co>
2015-03-08 13:33:22 -0700

    watching packages

--- transactions/watch_packages.py
+++ transactions/watch_packages.py
@@ -1,7 +1,7 @@
 import os
 import sys
 from django.conf import settings
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "blocbox.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "blocbox.blocbox.settings")
 #settings.configure(DEBUG=True, TEMPLATE_DEBUG=True,TEMPLATE_DIRS=('/yourprojet/templates',))
 from django.core.management import execute_from_command_line
 import datetime
