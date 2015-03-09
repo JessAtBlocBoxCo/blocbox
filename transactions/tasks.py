148914a5883e1e0e9143a9d5b7e011203dd9aec3
Jess <jess@blocbox.co>
2015-03-08 20:08:54 -0700

    __init__ changing from .__celery to __celery

--- transactions/tasks.py
+++ transactions/tasks.py
@@ -2,8 +2,12 @@
 from celery import Celery
 from django.conf import settings
 
-app = Celery('tasks', broker=BROKER_URL)
+app = Celery()
+#app = Celery('tasks', broker=BROKER_URL)
 
 @app.task
-def add(x, y):
-    return x + y
+def add(x, y):  return x + y
+
+
+if __name__ == '__main__':
+    app.worker_main()
