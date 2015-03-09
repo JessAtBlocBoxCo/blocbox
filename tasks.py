6eac810106bbe6214d92395f55894cff8dd0db37
Jess <jess@blocbox.co>
2015-03-09 16:20:36 -0700

    adding the celery init files

---
+++ tasks.py
@@ -0,0 +1,31 @@
+# http://docs.celeryproject.org/en/latest/getting-started/first-steps-with-celery.html
+from celery import Celery
+from celery import task
+from django.conf import settings
+from django.http import HttpResponse, Http404, HttpResponseRedirect
+from transactions.watch_packages import main as watch_packages_task
+from transactions.watch_packages import test_celery_beat
+from blocbox.__celery import app
+#app = Celery('blocbox')
+#app = Celery('tasks', broker=BROKER_URL)
+#app = Celery('tasks', backend='amqp', broker='amqp://guest@localhost:5672//')
+#app = Celery('tasks', backend='amqp', broker="amqp://jess:goodhood@localhost:5672/blocbox")
+
+@app.task
+def add(x, y):  return x + y
+
+
+@app.task
+def watch_packages():
+	  task = watch_packages_task()
+	  #return HttpResponse(task)
+	  return task   
+
+@app.task
+def test_schedule(enduserid, hostid, transid):
+    return test_celery_beat(enduserid, hostid, transid)
+
+
+
+if __name__ == '__main__':
+    app.worker_main()
