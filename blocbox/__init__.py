745a5b4fef10b54ec39acc12c59899c6ec88001b
Jess <jess@blocbox.co>
2015-03-08 19:58:47 -0700

    add celery task

--- blocbox/__init__.py
+++ blocbox/__init__.py
@@ -1,4 +1,4 @@
 from __future__ import absolute_import
 # This will make sure the app is always imported when Django starts so that shared_task will use this app.
 # from: http://docs.celeryproject.org/en/latest/django/first-steps-with-django.html
-from .celery import app as celery_app
+from .__celery import app as celery_app
