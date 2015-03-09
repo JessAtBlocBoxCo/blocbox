94148ff5f4f3894ab7b66937144aea104c2d896a
Jess <jess@blocbox.co>
2015-03-08 21:34:56 -0700

    b

--- transactions/tasks.py
+++ transactions/tasks.py
@@ -1,6 +1,7 @@
 # http://docs.celeryproject.org/en/latest/getting-started/first-steps-with-celery.html
 from celery import Celery
 from django.conf import settings
+from django.http import HttpResponse, Http404, HttpResponseRedirect
 app = Celery()
 #app = Celery('tasks', broker=BROKER_URL)
 
@@ -9,7 +10,9 @@ def add(x, y):  return x + y
 
 from watch_packages import main as watch_packages_task
 def watch_packages():
-    return watch_packages_task()
+	  task = watch_packages_task()
+	  return HttpResponse(task)
+    #return watch_packages_task()
     #return ("OK")
     #return response_messages_list    
 
