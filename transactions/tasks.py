8607a55babbba1c9dd44b3ab77e71d9421e567ad
Jess <jess@blocbox.co>
2015-03-08 20:41:11 -0700

    adding watch packages task to celery

--- transactions/tasks.py
+++ transactions/tasks.py
@@ -1,13 +1,17 @@
 # http://docs.celeryproject.org/en/latest/getting-started/first-steps-with-celery.html
 from celery import Celery
 from django.conf import settings
-
+from transanctions.watch_packages import watch_packages
 app = Celery()
 #app = Celery('tasks', broker=BROKER_URL)
 
 @app.task
 def add(x, y):  return x + y
 
+@app.ask
+def watch_packages_task:
+    watch_packages()
+    
 
 if __name__ == '__main__':
     app.worker_main()
