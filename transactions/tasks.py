d9a8a92b4bd460117bca1406b63f35b6bad01928
Jess <jess@blocbox.co>
2015-03-09 12:26:11 -0700

    celery

--- transactions/tasks.py
+++ transactions/tasks.py
@@ -6,7 +6,7 @@ from watch_packages import main as watch_packages_task
 from watch_packages import test_celery_beat
 #app = Celery()
 #app = Celery('tasks', broker=BROKER_URL)
-app = Celery('tasks', backend='amqp', broker='amqp://guest@localhost//')
+app = Celery('tasks', backend='amqp', broker='amqp://guest@localhost:5672//')
 
 @app.task
 def add(x, y):  return x + y
