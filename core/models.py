29729813fff3a839f7b6808454ccbc60299e5043
Jess <jess@blocbox.co>
2015-03-10 00:02:37 -0700

    changin task while running

--- core/models.py
+++ core/models.py
@@ -110,7 +110,8 @@ class UserInfo(AbstractBaseUser): #standard fields defined below
     notifyuser_blocboxnews = models.BooleanField(blank=True, default=True)
     notifyuser_trackinginfo = models.BooleanField(blank=True, default=True)
     notifyuser_newhostonblock = models.BooleanField(blank=True, default=True)
-
+		#zipcodes nearby - start this as a character string thats long
+		zipcodes_nearby = models.CharField(max_length=200, blank=True, null=True)
     #addingj custom or text fields
     customchar = models.CharField("Custom Character field to edit later", max_length=200, blank=True, null=True)
     customchartwo = models.CharField("Custom Character Field second version", max_length=200, blank=True, null=True)
