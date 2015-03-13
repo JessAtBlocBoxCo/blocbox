d0f89620680558dd2312814754da0b5f4b59de73
JB <john@blocbox.co>
2015-03-13 15:06:25 -0400

    didis

--- blocbox/static/admin/js/jbcustomization.js
+++ blocbox/static/admin/js/jbcustomization.js
@@ -14,7 +14,7 @@
     
 jQuery(document).ready(function($){
     //set animation timing
-    var animationDelay = 4000,
+    var animationDelay = 3500,
         //loading bar effect
         barAnimationDelay = 3800,
         barWaiting = barAnimationDelay - 3000, //3000 is the duration of the transition on the loading bar - set in the scss/css file
