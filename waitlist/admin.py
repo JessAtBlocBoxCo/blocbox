b71d0136ac7a9966b0368312cdf75dc8ca6f5dd3
Jess <jess@blocbox.co>
2015-04-11 15:22:59 -0700

    edit admin panel for waitlist

--- waitlist/admin.py
+++ waitlist/admin.py
@@ -5,7 +5,7 @@ from waitlist.models import Waitlist
 # Register your models here.
 class WaitlistAdmin(admin.ModelAdmin):
     #just list 'pass' if you want it to id just the email address and then they clik on it to see other fields
-    list_display = ('id', 'fulluser', 'email', 'zipcode', 'first_name', 'hostinterest', 'referred_by', )
+    list_display = ('id', 'fulluser', 'email', 'zipcode', 'first_name', 'hostinterest', 'referred_by', 'date_joined_waitlist' )
     list_filter = ['zipcode', 'fulluser' ]
     search_fields = ['email', 'first_name', 'zipcode']  
     
