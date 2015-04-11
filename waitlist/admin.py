f7092b8797d561f2dcc44fa1a8a32a28b8db037d
Jess <jess@blocbox.co>
2015-04-10 17:00:32 -0700

    no message for this commit sorry!

--- waitlist/admin.py
+++ waitlist/admin.py
@@ -5,10 +5,8 @@ from waitlist.models import Waitlist
 # Register your models here.
 class WaitlistAdmin(admin.ModelAdmin):
     #just list 'pass' if you want it to id just the email address and then they clik on it to see other fields
-    #pass
     list_display = ('id', 'fulluser', 'email', 'zipcode', 'first_name', 'hostinterest', 'referred_by', )
     list_filter = ['zipcode', 'fulluser' ]
     search_fields = ['email', 'first_name', 'zipcode']  
     
 admin.site.register(Waitlist, WaitlistAdmin)
-admin.site.unregister(Group)
