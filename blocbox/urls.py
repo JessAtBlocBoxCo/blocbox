6b96951e432e22a1bf6b448764a5805ca7b0355e
Jess <jess@blocbox.co>
2015-04-12 12:49:55 -0700

    edit join waitlist

--- blocbox/urls.py
+++ blocbox/urls.py
@@ -47,7 +47,7 @@ urlpatterns = patterns('',
     url(r'^joinwaitlist/$', views.joinwaitlist, name='joinwaitlist'),
     url(r'^joinwaitlist/referredby=(?P<referring_user_email>[^/]+)', views.joinwaitlist, name='joinwaitlist_referral'),
     url(r'^joinwaitlisttestform/$', views.joinwaitlist_testform, name='joinwaitlist_testform'),
-    #url(r'^joinwaitlisttestformpost/$', views.joinwaitlist_testformpost, name='joinwaitlist_testformpost'),
+    url(r'^joinwaitlisttestformpost/$', views.joinwaitlist_testformpost, name='joinwaitlist_testformpost'),
     url(r'^waitlistconfirmation/$', views.waitlist_confirmation, name='waitlist_confirmation'),
  		#NOTE - removing the caret ^ before register so blocbox/register calls this as well
  		#the carret sasy beginning must match, the $ says end of string must match
