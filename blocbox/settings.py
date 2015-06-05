be1032936e1e093c7b18920e2cbb6f9d1ae259e4
Jess <jess@blocbox.co>
2015-06-05 13:43:39 -0700

    removing south

--- blocbox/settings.py
+++ blocbox/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = (
     'django.contrib.staticfiles',
     'core',
     'transactions',
-    'south',   
+    #'south',   
     #'djcelery',
     #'kombu.transport.django',
     'password_reset',
