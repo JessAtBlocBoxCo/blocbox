bd381ee160e1309c36dd8dd4dcf9874d41b7917c
Jess <jess@blocbox.co>
2015-06-24 14:32:13 -0700

    making fre eoption

--- core/views.py
+++ core/views.py
@@ -448,9 +448,9 @@ def package_received_modal(request, confirm_id):
             trans.date_completed = datetoday
             trans.datetime_completed = datetimenow
             trans.save()
-    				#get last aftership stuff
-    		    courier = trans.shipment_courier
-    		    if courier:
+            #get last aftership stuff
+            courier = trans.shipment_courier
+            if courier:
                 slug = str(trans.shipment_courier.lower())
                 tracking = trans.tracking
                 datadict = api.trackings.get(slug, tracking)
