b755ef92177cd5e9d2d3f4af0d42845e8bbf727d
Jess <jess@blocbox.co>
2015-08-24 00:00:41 -0700

    editing auth

--- core/migrations/0004_alter_user_last_login_null.py
+++ core/migrations/0004_alter_user_last_login_null.py
@@ -8,7 +8,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('core', '0006_alter_user_username_opts'), #changing 'auth' to 'core'
+        ('core'), #'0006_alter_user_username_opts'), #changing 'auth' to 'core'
     ]
 
     operations = [
