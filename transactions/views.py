e820bfdd45c599a25f92e41ae6d56366d2b19c10
Jess <jess@blocbox.co>
2015-08-29 16:06:58 -0700

    fixing cal_form_submitted logic so that the if cal_form.is_valid method doesn't keep trying to vlaidate after submitted

--- transactions/views.py
+++ transactions/views.py
@@ -179,7 +179,7 @@ def startashipment(request, host_id=None, transaction_form_submitted=False, invo
     favortype='package'
     #transaction_form_submitted = False
     packagedays_count = None
-    cal_form_submitted = False
+    #cal_form_submitted = False
     if cal_form_submitted == False:     
         trans_form_package = None 
         packagedays = []  
