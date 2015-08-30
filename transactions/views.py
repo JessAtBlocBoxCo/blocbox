53ea1f20981b6bdd02796d7835de0235257d5697
Jess <jess@blocbox.co>
2015-08-29 21:32:47 -0700

    fixing cal_form_submitted logic so that the if cal_form.is_valid method doesn't keep trying to vlaidate after submitted

--- transactions/views.py
+++ transactions/views.py
@@ -53,9 +53,9 @@ def startashipment(request, host_id=None, transaction_form_submitted=False, invo
         amount_due = 0.00
         remaining_balance = balance - 1
     else:
-    	  if enduser.betauser_free:
+        if enduser.betauser_free:
     	      payment_needed = Fase
-    	  else:
+        else:
             payment_needed = True
         amount_due = None #this is processed on the payment page if they aren't applying account balance
         remaining_balance = None 
