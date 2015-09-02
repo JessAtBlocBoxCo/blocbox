0bf6a4df80c1b9324f8e06c6833f99efa1e904de
JB <john@blocbox.co>
2015-09-01 23:29:40 -0400

    alh

--- core/views.py
+++ core/views.py
@@ -295,9 +295,8 @@ def dashboard_host(request):
         shipments_in_transit = shipments_all_paid_notarchived_notcomplete.exclude(last_tracking_status="Delivered")
         #Shipments awaiting pickup
         shipments_waiting_pickup = shipments_all_paid_notarchived_notcomplete.filter(last_tracking_status="Delivered")
-        connections_all = Connection.objects.filter(host=thepersonviewingthepage) #JB - displays hosts connected to
-        connections_count = Connection.objects.filter(host=thepersonviewingthepage).count() #count them,removing status=0 after host_user=host
-        
+        connections_all = Connection.objects.filter(host_user=thepersonviewingthepage) #JB - displays hosts connected to
+        connections_count = Connection.objects.filter(host_user=thepersonviewingthepage).count() #count them,removing status=0 after host_user=host
     else: #if not authenticated set these to None
         transactions_all = None
         transactions_all_paid = None
@@ -308,6 +307,7 @@ def dashboard_host(request):
         shipments_complete_fordash = None
         shipments_in_transit = None
         shipments_waiting_pickup = None
+        connections_all = None
         connections_count = None
     return render(request, 'blocbox/dashboard-host.html', {
             'enduser':thepersonviewingthepage,
