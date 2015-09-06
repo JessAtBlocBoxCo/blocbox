7aeab76d00c898c5de0462c362439b0b3e55cd15
JB <john@blocbox.co>
2015-09-05 22:56:10 -0400

    polls

--- pollsjb/models.py
+++ pollsjb/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils import timezone
 
 
 class Question(models.Model):
@@ -7,6 +8,9 @@ class Question(models.Model):
     
     def __str__(self):
         return self.question_text
+    
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now( ) - datetime.timedelta(days=1)
 
 class Choice(models.Model):
     question = models.ForeignKey(Question)
