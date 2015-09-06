825487e1c7ddeb5540dc139ab09f68ea543c1289
JB <john@blocbox.co>
2015-09-05 20:53:17 -0400

    polls

--- pollsjb/models.py
+++ pollsjb/models.py
@@ -5,10 +5,9 @@ class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
 
-
-    class Choice(models.Model):
-        question = models.ForeignKey(Question)
-        choice_text = models.CharField(max_length=200)
-        votes = models.IntegerField(default=0)
+class Choice(models.Model):
+    question = models.ForeignKey(Question)
+    choice_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
     
     
