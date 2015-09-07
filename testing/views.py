36756520e24bfa9bee719ccfbced2e7b7b6e31d2
jess@blocbox.co <Jessica Yeats@NYO000122947002.ads.mckinsey.com>
2015-09-07 11:30:35 -0400

    host availability calendar

--- testing/views.py
+++ testing/views.py
@@ -398,6 +398,7 @@ def dashboard_host_test(request, host_id=None, trans=None, track_id=None, confir
                     conflict_new.date = str(nextmonth_calendar_year) + "-" + str(thismonth_num) + "-" + str(daynumber)  
                     conflict_new.save()                              
             cal_form_submitted = True
+            #test whether line 401 has error
         else:
             print cal_form.errors
     else:
