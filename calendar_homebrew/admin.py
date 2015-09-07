90481bac9a03c8c8d073993665fc84b67d2a1689
jess@blocbox.co <Jessica Yeats@NYO000122947002.ads.mckinsey.com>
2015-09-07 09:54:14 -0400

    host availability calendar

--- calendar_homebrew/admin.py
+++ calendar_homebrew/admin.py
@@ -1,5 +1,10 @@
 from django.contrib import admin
-from calendar_homebrew.models import HostConflicts_OldVersion, HostConflicts_BooleanVersion, HostWeeklyDefaultSchedule
+from calendar_homebrew.models import HostConflicts_DateVersion, HostConflicts_OldVersion, HostConflicts_BooleanVersion, HostWeeklyDefaultSchedule
+
+class ConflictsAdmin (admin.ModelAdmin): 
+    list_display = ('id', 'host', )
+    list_filter = ['host',  ]
+    search_fields = ['host', ]  
 
 class ConflictsAdmin_BooleanVersion(admin.ModelAdmin): 
     list_display = ('id', 'host', )
@@ -15,7 +20,8 @@ class WeeklyScheduleAdmin(admin.ModelAdmin):
     list_display = ('id', 'host', 'monday_available', 'tuesday_available', 'wednesday_available', 'thursday_available', 'friday_available', 'saturday_available', 'sunday_available',)
     list_filter = ['host',]
     search_fields = ['host',  ] 
-    
+
+admin.site.register(HostConflicts_DateVersion, ConflictsAdmin)
 admin.site.register(HostConflicts_BooleanVersion, ConflictsAdmin_BooleanVersion)
 admin.site.register(HostConflicts_OldVersion, ConflictsAdmin_OldVersion)
 admin.site.register(HostWeeklyDefaultSchedule, WeeklyScheduleAdmin)
