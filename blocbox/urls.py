bcad01e423dc773569c1e6c54d31812124d92c7e
JB <john@blocbox.co>
2015-09-12 16:24:20 -0400

    adskj

--- blocbox/urls.py
+++ blocbox/urls.py
@@ -57,6 +57,7 @@ urlpatterns = patterns('',
                        
     url(r'^hostdashboard/$',views.dashboard_host, name='host_dashboard'),
     url(r'^hostdashboard/confirm_id(?P<confirm_id>\d+)/',views.dashboard_host, name='host_dashboard'),
+    url(r'^hostdashboard/issue_id(?P<issue_id>\d+)/', views.dashboard, name='host_dashboard'),   
                        
     
 
